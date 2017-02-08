0ac1b2feec20e60d4bfff644270497ed7faf5004
mprep-btc <mprep.throwaway@gmail.com>
2017-02-08 13:12:17 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Bitcointalk-Post-Iconizer
-Userscript that allows you to add custom post and PM editing buttons
+# Bitcointalk Post Iconizer
+
+![Screenshot 1](/screenshots/screenshot1.PNG?raw=true "")
+---
+Userscript, which adds the ability to create custom buttons for the post and PM creation pages on Bitcointalk (and possibly other SMF 1.X forums).
+
+
+## Installation instructions
+
+1. Get a userscript manager plugin if your browser doesn't support userscripts natively (or if these scripts don't work for some reason);
+  * Examples:
+    * [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) for Firefox.
+    * [Tampermokey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) for Chrome.
+2. Install the main userscript found [here](/customIcons.js?raw=true);
+3. Install any plugin userscripts (official plugins can be found [here](/plugins); click on the plugin and then click the button 'Raw' to get the file).
+4. Make sure the main userscript is executed _**before**_ any plugins are (your userscript manager should allow you to control the execution order of the plugins).
+5. *(Optional) Move around the plugin execution order in order to choose which plugins should be loaded first.*
+6. *(Optional) Create your own plugins by using the function refernce located in the main userscript*
+
+
+## Examples (for plugins)
+
+#### Creating a button that inserts BB code:
+``` javascript
+addInserter("someIcon.jpg","[b]Example things[/b]","Thing name");
+```
+
+#### Creating a button that surrounds your selection with BB code:
+``` javascript
+addSurrounder("someIcon.jpg","[b][u]start"," and the end[/u][/b]","Thing name");
+```
+
+## License
+The userscript and all plugin userscripts are provided under the [AGPL 3.0 license](https://github.com/mprep-btc/Bitcointalk-Post-Iconizer/blob/master/LICENSE).
+
+## Changelog
+
+v0.2 - refractored code, added custom button and separator inserters, added update URLs
+
+v0.1 - initial release.
---
+++ customIcons.js
@@ -0,0 +1,93 @@
+// ==UserScript==
+// @name        Bitcointalk Post Iconizer
+// @namespace   mprep
+// @description Add custom buttons to Bitcointalk's post editing page for inserting BB code
+// @include     https://bitcointalk.org/index.php?action=post;*
+// @include     https://bitcointalk.org/index.php?action=pm;sa=send*
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
+// @version     0.21
+// @downloadURL   https://raw.githubusercontent.com/mprep-btc/Bitcointalk-Post-Iconizer/master/customIcons.js
+// @grant       none
+// ==/UserScript==
+
+//Adding a pivot for other buttons
+if ($('.custom-iconizer').length == 0) 
+{
+    $('img[src="https://bitcointalk.org/Themes/custom1/images/bbc/list.gif"]').parent().
+        after('<img src="https://bitcointalk.org/Themes/custom1/images/bbc/divider.gif" alt="|" style="margin: 0 3px 0 3px;" class="custom-iconizer">');
+}
+
+/************************************** ⌄⌄ API FUNCTIONS ⌄⌄ *****************************************/
+
+/* Adds inserter (e.g. [btc]) button
+ *
+ * Parameters:
+ *  [imageUrl] - URL / data URI for the button's icon;
+ *  [bbcode] - the text you want to insert in the textbox;
+ *  [bbStart] - the text you want to add after the text selected in the textbox;
+ *  [title] - the text you want displayed when the user hovers over the buttons
+ *  [id] (optional) - button's ID for any custom extensions.
+ */
+function addInserter(imageUrl, bbcode, title, id='icon') 
+{
+    $('.custom-iconizer:last').after('<a class="custom-iconizer" href="javascript:void(0);" onclick="replaceText(\'' +
+        bbcode + '\', document.forms.postmodify.message); return false;"><img onmouseover="bbc_highlight(this, true);" onmouseout="if (window.bbc_highlight) bbc_highlight(this, false);" src="' +
+        imageUrl + '" alt="Insert ' +
+        title + ' symbol" title="Insert ' +
+        title + ' symbol" style="background-image: url(https://bitcointalk.org/Themes/custom1/images/bbc/bbc_bg.gif); margin: 1px 2px 1px 1px;" align="bottom" height="22" width="23"></a>');
+}
+
+/* Adds surrounder (e.g. [b]some selected text here[/b]) button
+ *
+ * Parameters:
+ *  [imageUrl] - URL / data URI for the button's icon;
+ *  [bbStart] - the text you want to add before the text selected in the textbox;
+ *  [bbStart] - the text you want to add after the text selected in the textbox;
+ *  [title] - the text you want displayed when the user hovers over the buttons
+ *  [id] (optional) - button's ID for any custom extensions.
+ */
+function addSurrounder(imageUrl, bbStart, bbEnd, title, id='icon') 
+{
+    $('.custom-iconizer:last').after('<a class="custom-iconizer" href="javascript:void(0);" onclick="surroundText(' + 
+    bbStart + ',' + 
+    bbEnd + ', document.forms.postmodify.message); return false;"><img onmouseover="bbc_highlight(this, true);" onmouseout="if (window.bbc_highlight) bbc_highlight(this, false);" src="' + 
+    imageUrl + '" alt="' + 
+    title + '" title="' + 
+    title + '" style="background-image: url(https://bitcointalk.org/Themes/custom1/images/bbc/bbc_bg.gif); margin: 1px 2px 1px 1px;" align="bottom" height="22" width="23"></a>');
+}
+
+/* Adds separator
+ *
+ * Parameters:
+ *  [id] (optional) - separator's ID for any custom extensions.
+ */
+function addSeparator(id='icon') 
+{
+    $('.custom-iconizer:last').after('<img src="https://bitcointalk.org/Themes/custom1/images/bbc/divider.gif" alt="|" style="margin: 0 3px 0 3px;" class="custom-iconizer">');
+}
+
+/* Adds button with a custom onclick function
+ *
+ * Parameters:
+ *  [imageUrl] - URL / data URI for the button's icon;
+ *  [customFunc] - the function you want to call when the buttons clicked;
+ *  [title] - the text you want displayed when the user hovers over the buttons
+ *  [id] (optional) - button's ID for any custom extensions.
+ */
+function addUtilitizer(imageUrl, customFunc, title, id='icon') 
+{
+    $('.custom-iconizer:last').after('<a class="custom-iconizer" id='+
+    id+' href="javascript:void(0);" onclick="' + 
+    customFunc + '"><img onmouseover="bbc_highlight(this, true);" onmouseout="if (window.bbc_highlight) bbc_highlight(this, false);" src="' + 
+    imageUrl + '" alt="' + 
+    title + '" title="' + 
+    title + '" style="background-image: url(https://bitcointalk.org/Themes/custom1/images/bbc/bbc_bg.gif); margin: 1px 2px 1px 1px;" align="bottom" height="22" width="23"></a>');
+}
+
+/************************************** ^^ API FUNCTIONS ^^ *****************************************/
+
+
+//Inject API scripts into page
+var script = document.createElement('script');
+script.appendChild(document.createTextNode(addInserter + addSurrounder + addSeparator + addUtilitizer));
+(document.body || document.head || document.documentElement).appendChild(script);
