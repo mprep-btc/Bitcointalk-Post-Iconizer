d19e9fb9f4eb0540ea64ae299d89239f7d9fc353
mprep <mprep.throwaway@gmail.com>
2017-02-08 19:54:53 +0200

    Small core fixes, updated altcoin plugin

--- customIcons.js
+++ customIcons.js
@@ -5,8 +5,8 @@
 // @include     https://bitcointalk.org/index.php?action=post;*
 // @include     https://bitcointalk.org/index.php?action=pm;sa=send*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
-// @version     0.21
-// @downloadURL   https://raw.githubusercontent.com/mprep-btc/Bitcointalk-Post-Iconizer/master/customIcons.js
+// @version     0.22
+// @downloadURL https://raw.githubusercontent.com/mprep-btc/Bitcointalk-Post-Iconizer/master/customIcons.js
 // @grant       none
 // ==/UserScript==
 
@@ -32,9 +32,9 @@ function addInserter(imageUrl, bbcode, title, id='icon')
 {
     $('.custom-iconizer:last').after('<a class="custom-iconizer" href="javascript:void(0);" onclick="replaceText(\'' +
         bbcode + '\', document.forms.postmodify.message); return false;"><img onmouseover="bbc_highlight(this, true);" onmouseout="if (window.bbc_highlight) bbc_highlight(this, false);" src="' +
-        imageUrl + '" alt="Insert ' +
-        title + ' symbol" title="Insert ' +
-        title + ' symbol" style="background-image: url(https://bitcointalk.org/Themes/custom1/images/bbc/bbc_bg.gif); margin: 1px 2px 1px 1px;" align="bottom" height="22" width="23"></a>');
+        imageUrl + '" alt="' +
+        title + '" title="' +
+        title + '" style="background-image: url(https://bitcointalk.org/Themes/custom1/images/bbc/bbc_bg.gif); margin: 1px 2px 1px 1px;" align="bottom" height="22" width="23"></a>');
 }
 
 /* Adds surrounder (e.g. [b]some selected text here[/b]) button
